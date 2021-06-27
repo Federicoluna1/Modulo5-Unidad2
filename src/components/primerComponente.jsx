567b0e87a2c27e9d5c6e7a80cd54fa0879c2cdb4
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-06-27 20:37:29 -0300

    Jsx with the component

--- src/components/primerComponente.jsx
+++ src/components/primerComponente.jsx
@@ -1,12 +1,13 @@
 import '../App.css';
+import React from 'react';
 
 const PrimerComponente = (props) => {
-const {url, tittle, description} = props
+const {url, title, description} = props
     const element = 
         <div className='container'> 
-            <img src={url}></img>
+            <img src={url} alt=''></img>
                 <div>
-                    <h2 className = 'container-tittle'>{tittle}</h2>
+                    <h2 className = 'container-title'>{title}</h2>
                     <p className = 'container-description'>{description}</p>
                 </div>
         </div>;
